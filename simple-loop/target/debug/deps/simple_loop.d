ceca7a492144b2c373ef227636a3f0960118bc60
RetroJoshua <joshua537.nit@gmail.com>
2025-08-21 11:40:10 +0530

    simple loop

---
+++ simple-loop/target/debug/deps/simple_loop.d
@@ -0,0 +1,5 @@
+C:\Users\user\rust-basics\simple-loop\target\debug\deps\simple_loop.d: src\main.rs
+
+C:\Users\user\rust-basics\simple-loop\target\debug\deps\simple_loop.exe: src\main.rs
+
+src\main.rs:
