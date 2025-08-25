af9f7733c165d8cd56258fc5e6d9dd9b0fd85cbc
RetroJoshua <joshua537.nit@gmail.com>
2025-08-25 20:23:09 +0530

    complex-loop

---
+++ loop-through-collection-1/target/debug/deps/loop_through_collection_1.d
@@ -0,0 +1,5 @@
+C:\Users\user\rust-basics\loop-through-collection-1\target\debug\deps\loop_through_collection_1.d: src\main.rs
+
+C:\Users\user\rust-basics\loop-through-collection-1\target\debug\deps\loop_through_collection_1.exe: src\main.rs
+
+src\main.rs:
---
+++ loop-through-collection-1/target/debug/loop-through-collection-1.d
@@ -0,0 +1,1 @@
+C:\Users\user\rust-basics\loop-through-collection-1\target\debug\loop-through-collection-1.exe: C:\Users\user\rust-basics\loop-through-collection-1\src\main.rs
